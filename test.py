603b28eef4c68a4506488ebfca9cdd5962547b98
Attila Oláh <atl@google.com>
2020-05-12 13:10:15 +0200

    Patch up test.py so that it would at least run.

--- test.py
+++ test.py
@@ -2,46 +2,50 @@
 Author: Wei Wang
 """
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import cv2
 import math
 import numpy as np
 import torch
 
-from utils import *
-from model import *
-from datsetprocess import *
+import models
+import utils
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+PIXEL_MAX = 1
 
 
 def psnr2(img1, img2):
     mse = np.mean((img1/255. - img2/255.) ** 2)
     if mse < 1.0e-10:
         return 100
-    PIXEL_MAX = 1
     return 20 * math.log10(PIXEL_MAX / math.sqrt(mse))
 
 
-scene_directory = 'F:\project\AHDRNet\data\Test\PAPER\ManStanding'
+scene_directory = os.path.join('data', 'Test', 'PAPER', 'ManStanding')
 
 
-def inference():
+def inference() -> None:
     # Load the image
     # Read Expo times in scene
-    expoTimes = ReadExpoTimes(os.path.join(scene_directory, 'exposure.txt'))
+    expoTimes = utils.ReadExpoTimes(os.path.join(scene_directory,
+                                                 'exposure.txt'))
     # Read Image in scene
-    imgs = ReadImages(list_all_files_sorted(scene_directory, '.tif'))
+    imgs = utils.ReadImages(utils.list_all_files_sorted(scene_directory,
+                                                        '.tif'))
     # Read label
-    label = ReadLabel(scene_directory)
+    label = utils.ReadLabel(scene_directory)
     # inputs-process
-    pre_img0 = LDR_to_HDR(imgs[0], expoTimes[0], 2.2)
-    pre_img1 = LDR_to_HDR(imgs[1], expoTimes[1], 2.2)
-    pre_img2 = LDR_to_HDR(imgs[2], expoTimes[2], 2.2)
+    pre_img0 = utils.LDR_to_HDR(imgs[0], expoTimes[0], 2.2)
+    pre_img1 = utils.LDR_to_HDR(imgs[1], expoTimes[1], 2.2)
+    pre_img2 = utils.LDR_to_HDR(imgs[2], expoTimes[2], 2.2)
     output0 = np.concatenate((imgs[0], pre_img0), 2)
     output1 = np.concatenate((imgs[1], pre_img1), 2)
     output2 = np.concatenate((imgs[2], pre_img2), 2)
     # label-process
-    label = range_compressor(label)*255.0
+    label = utils.range_compressor(label)*255.0
 
     im1 = torch.Tensor(output0).cuda()
     im1 = torch.unsqueeze(im1, 0).permute(0, 3, 1, 2)
@@ -53,9 +57,9 @@ def inference():
     im3 = torch.unsqueeze(im3, 0).permute(0, 3, 1, 2)
 
     # Load the pre-trained model
-    model = AHDRNet().cuda()
+    model = models.AHDRNet().cuda()
     model.eval()
-    model.load_state_dict(torch.load('./Model/2300.pkl'))
+    model.load_state_dict(torch.load('train_results/model/latest.pkl')['model'])
 
     # Run
     with torch.no_grad():
@@ -65,11 +69,10 @@ def inference():
     pre = pre.data[0].cpu().numpy()
     pre = np.clip(pre * 255.0, 0., 255.)
     pre = pre.transpose(1, 2, 0)
-    #pre1=cv2.cvtColor(pre, cv2.COLOR_RGB2BGR)
     p = psnr2(pre, label)
     print(p)
-    #cv2.imwrite('./recover/PeopleTalking/out.png', pre)
-    #cv2.imwrite('./recover/PeopleTalking/label.png', label)
+    cv2.imwrite('./recover/PeopleTalking/out.png', pre)
+    cv2.imwrite('./recover/PeopleTalking/label.png', label)
 
 
 if __name__ == '__main__':
